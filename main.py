f169a888107e6444f44d861c67bbddb3716570b7
jkpark8658 <jkparkusu@gmail.com>
2025-09-25 20:29:46 +0900

    MakeCode 프로젝트용 기초 파일

---
+++ main.py
@@ -0,0 +1,10 @@
+A = 1
+S = 0
+while A <= 3:
+    S = S + A
+    A = A + 1
+basic.show_number(S)
+
+def on_forever():
+    pass
+basic.forever(on_forever)
---
+++ main.ts
@@ -0,0 +1,10 @@
+let A = 1
+let S = 0
+while (A <= 100) {
+    S = S + A
+    A = A + 1
+}
+basic.showNumber(S)
+basic.forever(function () {
+	
+})
